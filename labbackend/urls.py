ae3b9a7b1da4927b1ea406c3903f1bca3e31e716
SMRFT <110091550+SMRFT@users.noreply.github.com>
2025-03-25 09:29:17 +0530

    Update urls.py

--- labbackend/urls.py
+++ labbackend/urls.py
@@ -1,11 +1,18 @@
 #urls.py
 from django.urls import path
 from . import views
+from .views import generate_invoice, get_invoices,update_invoice,delete_invoice
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from . import whatsapp
 urlpatterns = [
     path('registration/', views.registration, name='registration'),
     path('login/', views.login, name='login'),
     path('patient/create/', views.create_patient, name='create_patient'),
     path('latest-patient-id/', views.get_latest_patient_id, name='get_latest_patient_id'),
+    path("patient/update/<str:patient_id>/", views.update_patient, name="update_patient"),
     path('patients_get_barcode/', views.get_barcode_by_date, name='get_barcode_by_date'),
     path('get-max-barcode/', views.get_max_barcode, name='get_max_barcode'),
     path('save-barcodes/', views.save_barcodes, name='save_barcodes'),
@@ -44,18 +51,39 @@ urlpatterns = [
     path('patient_test_status/', views.patient_test_status, name='patient_test_status'),
     path('all-patients/', views.get_all_patients, name='get_all_patients'),
     path('overall_report/', views.overall_report, name='overall_report'),
+    path('patient_test_sorting/', views.patient_test_sorting, name='patient_test_sorting'),
     path('credit_amount/<str:patient_id>/', views.credit_amount_update, name='credit_amount_update'),
     path('update-credit/<str:patient_id>/', views.update_credit_amount, name='update_credit_amount'),
     path('send-email/', views.send_email, name='send_email'),
     path('SalesVisitLog/', views.salesvisitlog, name='salesvisitlog'),
+    path('SalesVisitLogReport/', views.get_sales_log, name='sales-visit-log-report'),
     path('hospitallabform/', views.hospitallabform, name='hospitallabform'),
     path('save-logistic-data/', views.save_logistic_data, name='save-logistic-data'),
     path('get_logistic_data/', views.get_logistic_data, name='get_logistic_data'),
     path('patient/get/<str:patient_id>/', views.get_patient_by_id, name='get_patient_by_id'),
     path('getlogisticdata/', views.getlogisticdatabydate, name='getlogisticdatabydate'),
+    path('check-barcode/', views.check_barcode, name='check-barcode'),
     # path('getsamplecollectordetails/', views.getsamplecollectordetails, name='getsamplecollectordetails'),
     path('savesamplecollector/', views.savesamplecollectordetails, name='savesamplecollectordetails'),
     path('updatesamplecollectordetails/', views.update_sample_collector_details, name='update_sample_collector_details'),
     path('patient/get/<str:patient_id>/', views.get_patient_by_id, name='get_patient_by_id'),
     path('consolidated-data/', views.ConsolidatedDataView.as_view(), name='consolidated_data'),
+    path("generate-invoice/", generate_invoice, name="generate-invoice"),
+    path("get-invoices/", get_invoices, name="get-invoices"),
+    path("update-invoice/<str:invoice_number>/", update_invoice, name="update-invoice"),
+    path("delete-invoice/<str:invoice_id>/", delete_invoice, name="delete-invoice"),
+    path('salesdashboard/', views.salesdashboard, name='salesdashboard'),
+    path('getsalesmapping/',views.getsalesmapping, name='getsalesmapping'),
+    path('logisticdashboard/', views.logisticdashboard, name='logisticdashboard'),
+    path('search_refund/', views.search_refund, name='search_refund'),
+    path('verify_and_process_refund/', views.verify_and_process_refund, name='verify_and_process_refund'),
+    path('generate_otp/', views.generate_otp, name='generate_otp'),
+    path('search_cancellation/', views.search_cancellation, name='search_cancellation'),
+    path("upload-pdf/", whatsapp.upload_pdf_to_gridfs, name="upload_pdf"),
+    path("get-file/<str:file_id>/", whatsapp.get_pdf_from_gridfs, name="get_pdf"),
+    path("send-whatsapp/", whatsapp.send_whatsapp_message, name="send_whatsapp"),
+    path('get_patients/', views.get_patients, name='get_patients'),
+    path('patient/update_billing/<str:patient_id>/', views.update_billing, name='update_billing'),
+    path('patient/tests/<str:patient_id>/', views.get_patient_tests, name='get_patient_tests'),
+
 ]
